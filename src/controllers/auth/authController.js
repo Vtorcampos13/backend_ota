8976b9a7392b45017356710ee35bb7bcfb316cd6
albinramos <dramosh79@gmail.com>
2023-10-31 17:34:18 +0100

    conflictos solucionados

--- src/controllers/auth/authController.js
+++ src/controllers/auth/authController.js
@@ -1,12 +1,3 @@
-import jwt from "jsonwebtoken";
-
-/* const loginApi = (req,res) => {
-    const {username,password} = req.query;
-    const token = jwt.sign({user:username},process.env.JSON_SECRET,
-    {expiresIn:"10min"});
-    req.json({token});
-} */
-
 const login = (req,res) => {
     const {matricula,password} = req.body;
     //... comprobar que usuario existe y contraseña correcta
